ae4a3ee86e0f8b9e78b811c100852e4152a61985
fady <fady.tarek.1995@gmail.com>
2024-08-21 13:14:05 +0300

    Move data to service so components handle the ui only

--- src/app/productlist/productlist.component.ts
+++ src/app/productlist/productlist.component.ts
@@ -3,6 +3,7 @@ import { product } from '../models/product';
 import * as data from '../../assets/items_data.json';
 import { NgFor } from '@angular/common';
 import { ProductItemComponent } from '../product-item/product-item.component';
+import { ProductsdataService } from '../services/productsdata.service';
 
 @Component({
   selector: 'app-productlist',
@@ -14,66 +15,14 @@ import { ProductItemComponent } from '../product-item/product-item.component';
 export class ProductlistComponent {
   title: string = 'Home Page'
   productItems : product[] = [];
-  cartItems : product[] = []; 
-  constructor() { }
+  
+  constructor(private productservice :ProductsdataService) {}
 
   addtocart(p:product) : void{
-    this.cartItems.push(p);
-    this.cartItems.forEach(element => {
-      console.log(element.amount +": " + element.name +  " added to the cart !" );
-    });
+    this.productservice.addtocart(p);
   }
 
   ngOnInit(): void {
-    //console.log( JSON.stringify(data));
-
-    this.productItems =[
-      {
-          "id": 1,
-          "name": "Book",
-          "price": 9.99,
-          "url": "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-          "description": "You can read it!"
-        },
-        {
-          "id": 2,
-          "name": "Headphones",
-          "price": 249.99,
-          "url": "https://images.unsplash.com/photo-1583394838336-acd977736f90?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-          "description": "Listen to stuff!"
-        },
-        {
-          "id": 3,
-          "name": "Backpack",
-          "price": 79.99,
-          "url": "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-          "description": "Carry things around town!"
-        },
-        {
-          "id": 4,
-          "name": "Glasses",
-          "price": 129.99,
-          "url": "https://images.unsplash.com/photo-1591076482161-42ce6da69f67?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-          "description": "Now you can see!"
-        },
-        {
-          "id": 5,
-          "name": "Cup",
-          "price": 4.99,
-          "url": "https://images.unsplash.com/photo-1517256064527-09c73fc73e38?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-          "description": "Drink anything with it!"
-        },
-        {
-          "id": 6,
-          "name": "Shirt",
-          "price": 29.99,
-          "url": "https://images.unsplash.com/photo-1581655353564-df123a1eb820?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80",
-          "description": "Wear it with style!"
-      }
-    ]; 
-    
-    this.productItems.forEach(element => {
-      element.amount = 0;
-    });
+    this.productItems = this.productservice.getproducts();
   }
 }
