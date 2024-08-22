277e47ef3aabe33ffc622a20b8a96464dc0bdca6
fady <fady.tarek.1995@gmail.com>
2024-08-22 17:48:41 +0300

    added user form data to cart component and created a variable to calculate total price , save it to service

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -3,20 +3,29 @@ import { product } from '../models/product';
 import { ProductsdataService } from '../services/productsdata.service';
 import { NgFor } from '@angular/common';
 import { ProductItemComponent } from '../product-item/product-item.component';
+import { UserdetailsComponent } from '../userdetails/userdetails.component';
+
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [NgFor , ProductItemComponent],
+  imports: [NgFor , ProductItemComponent , UserdetailsComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
 
   productItems : product[] = [];
-  
+  totalprice:number=0;
+
   constructor(private productservice : ProductsdataService){}
   
   ngOnInit(): void {
     this.productItems = this.productservice.cartItems;
+    this.productItems.forEach(element => {
+      if(element.amount){
+        this.totalprice += element.price * element.amount ;
+      }
+    });
+    this.productservice.settotalprice(this.totalprice);
   }
 }
