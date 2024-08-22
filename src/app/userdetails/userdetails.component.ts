b80d30ef8544c09f5d74e8cc9cb80cdc878183d9
fady <fady.tarek.1995@gmail.com>
2024-08-22 17:46:03 +0300

    added form and validate input

---
+++ src/app/userdetails/userdetails.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { user } from '../models/user';
+import { FormsModule } from '@angular/forms';
+import { ProductsdataService } from '../services/productsdata.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-userdetails',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './userdetails.component.html',
+  styleUrl: './userdetails.component.css'
+})
+export class UserdetailsComponent {
+  name:string='';
+  address:string = '';
+  cardnumber:string='' ;
+
+  constructor(private productservice :ProductsdataService , private router: Router){}
+
+  placeOrder():void{
+    console.log(this.name + " : " + this.address + " : " + this.cardnumber);
+    this.productservice.saveuserdata({name:this.name , address:this.address , cardnumber: this.cardnumber });
+    this.router.navigate(['/orderplaced']);
+  }
+
+}
