613f5a21ba970f8793d8b206fb35515cfcd1a4e0
fady <fady.tarek.1995@gmail.com>
2024-08-23 02:51:26 +0300

    fetch data from local json file using http request instead of static data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet  } from '@angular/router';
 import { ProductlistComponent } from './productlist/productlist.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , ProductlistComponent , NavbarComponent , FormsModule],
+  imports: [HttpClientModule ,RouterOutlet , ProductlistComponent , NavbarComponent , FormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/services/productsdata.service.ts
+++ src/app/services/productsdata.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { product } from '../models/product';
 import { user } from '../models/user';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,7 @@ export class ProductsdataService {
   currentItem:number = 0;
   totalprice:number = 0;
 
-  constructor() {
+  constructor(private httpclient:HttpClient) {
     this.userdata = {
       name:'',
       address:'',
@@ -51,55 +53,19 @@ export class ProductsdataService {
   getCurrentItem(){
     return this.productItems[this.currentItem];
   }
-  
+  getproductdatafromjsonfile():Observable<product[]>{
+    return this.httpclient.get<product[]>('../assets/items_data.json');
+  }
   getproducts(){
-    this.productItems =[
-      {
-          "id": 1,
-          "name": "Book",
-          "price": 9.99,
-          "url": "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-          "description": "You can read it!"
-        },
-        {
-          "id": 2,
-          "name": "Headphones",
-          "price": 249.99,
-          "url": "https://images.unsplash.com/photo-1583394838336-acd977736f90?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-          "description": "Listen to stuff!"
-        },
-        {
-          "id": 3,
-          "name": "Backpack",
-          "price": 79.99,
-          "url": "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-          "description": "Carry things around town!"
-        },
-        {
-          "id": 4,
-          "name": "Glasses",
-          "price": 129.99,
-          "url": "https://images.unsplash.com/photo-1591076482161-42ce6da69f67?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-          "description": "Now you can see!"
-        },
-        {
-          "id": 5,
-          "name": "Cup",
-          "price": 4.99,
-          "url": "https://images.unsplash.com/photo-1517256064527-09c73fc73e38?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-          "description": "Drink anything with it!"
-        },
-        {
-          "id": 6,
-          "name": "Shirt",
-          "price": 29.99,
-          "url": "https://images.unsplash.com/photo-1581655353564-df123a1eb820?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80",
-          "description": "Wear it with style!"
-      }
-    ]; 
 
-    this.productItems.forEach(element => {
-      element.amount = 0;
+    this.getproductdatafromjsonfile().subscribe(res =>{
+      for (let index = 0; index < res.length; index++) {
+
+        const product = res[index];
+        product['amount'] = 0;
+        this.productItems.push(product);
+
+      }
     });
 
     return this.productItems;
